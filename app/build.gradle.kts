b2ddd9c5f80cdf0460667da89f829eabfe8ef810
whatrushik <82358369+topanim@users.noreply.github.com>
2025-09-15 00:43:10 +0300

    ♻️ refactor(build): migrate to stable API in all modules
    - Replaced deprecated methods with stable alternatives

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,8 +67,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        jvmToolchain(11)
     }
 
     buildFeatures {
--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        jvmToolchain(11)
     }
     buildFeatures {
         compose = true
