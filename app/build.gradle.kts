33306d981e8a286c58b629fa7a3001011da5ff4f
whatrushik <82358369+topanim@users.noreply.github.com>
2026-04-11 21:16:49 +0300

    🚀 release: 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = generateVersionCode()
-        versionName = "1.0.4-beta.1"
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
